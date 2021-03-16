8f90bb49be972d5e649cf1e70a79afd3733d780e
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-16 14:23:24 +0700

    tugas
    Ah buat belajar

---
+++ Minggu2/Latihan/Latihan1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu2_latihan;
+
+/**
+ *
+ * @author Fikri
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Belajar java");
+        System.out.println("ini kode java"
+                + "");
+        
+        System.out.print("hello");
+        System.out.print("world");
+    }
+    
+}
