5d0ae87e6e6c859349dd4387ffb3bbfa70f0ab60
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-16 20:44:25 +0700

    Create tugas3.java

---
+++ Minggu2/Tugas/tugas3.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu2_tugas;
+
+/**
+ *
+ * @author Fikri
+ */
+public class tugas3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean GameOver = false;
+        int student=50,classes=3;
+        //benar
+        double sales_tax;
+        //benar
+        short number1;
+        //benar
+        
+//        int 2beOrNot2be;
+//        //salah karena berisi karakter selain angka
+//        float price index;
+//        // salah karena ada spasi diantara nama variabel
+//        double lastyear'sPrice;
+//        // salah karena ada ' diantara nama variabel
+//        long class;
+//        // salah karena memakai nama class
+    }
+    
+}
