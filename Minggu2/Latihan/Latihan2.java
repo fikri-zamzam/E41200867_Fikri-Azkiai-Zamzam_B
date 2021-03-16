ea410bb38df65372cfd1397c3db917e72f17456a
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-16 20:44:05 +0700

    Create Latihan2.java

---
+++ Minggu2/Latihan/Latihan2.java
@@ -0,0 +1,28 @@
+package Minggu2_latihan;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fikri
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println(nama);
+        System.out.println(a);
+    }
+    
+}
