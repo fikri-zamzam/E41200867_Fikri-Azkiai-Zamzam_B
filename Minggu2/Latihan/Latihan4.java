d85c1971e89ff46521cf30a86fe05aef2d951db6
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-16 20:44:13 +0700

    Create Latihan4.java

---
+++ Minggu2/Latihan/Latihan4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu2_latihan;
+import java.util.Scanner;
+/**
+ *
+ * @author Fikri
+ */
+public class Latihan4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a,b,c = 0;
+        System.out.println("masukan angka1 :");
+        a = scan.nextInt();
+        System.out.println("masukan angka2 :");
+        b = scan.nextInt();
+        c = a+b;
+        System.out.println("angka 1 ="+a);
+        System.out.println("angka 2 ="+b);
+        System.out.println("hasil = "+c);
+                
+    }
+    
+}
