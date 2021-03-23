96491e5f6d4e56600bbf0d9188aa31789fd352a2
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-23 20:00:03 +0700

    Create Latihan2_3.java

---
+++ Minggu3/Latihan/Latihan2_3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu3_Latihan;
+
+/**
+ *
+ * @author Fikri
+ */
+public class Latihan2_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] angka2 = {4,2,1,45,6};
+        String [] text = {"Saya","Sedang","Belajar","OOP","Java",};
+        
+        for (int i = 0; i < angka2.length; i++) {
+            System.out.println(angka2[i]);
+        }
+        
+        for (int j = 0; j < text.length; j++) {
+            System.out.print(text[j]);
+            System.out.print(" ");
+        }
+    }
+    
+}
