6d27a4380c27ba09d34d08dbd16656acf0ed4e35
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-23 20:00:20 +0700

    Create Tugas1_3.java

---
+++ Minggu3/Tugas/Tugas1_3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu3_Tugas;
+import java.util.Scanner;
+/**
+ *
+ * @author Fikri
+ */
+public class Tugas1_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String [] nama = {"Fikri","Azkia'i","Zamzam"};
+        
+        for (int i = 0; i < nama.length; i++) {
+            System.out.print(nama[i]+" ");
+        }
+        System.out.println("");
+    }
+    
+}
