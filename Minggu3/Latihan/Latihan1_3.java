8e2bc8fb735b46eb029906c79644b173e336853b
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-23 19:59:54 +0700

    Create Latihan1_3.java

---
+++ Minggu3/Latihan/Latihan1_3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu3_Latihan;
+
+/**
+ *
+ * @author Fikri
+ */
+public class Latihan1_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] angka = {3,3,1,4,3,24,2};
+        
+        System.out.println("Jumlah elemen array angka adalah = "+angka.length);
+    }
+    
+}
