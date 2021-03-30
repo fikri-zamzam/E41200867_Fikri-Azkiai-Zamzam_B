a39ab58ac5b3f2c27486f5e7df56380a0f9a3d77
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-30 19:11:14 +0700

    Create Tugas3_4.java

---
+++ Minggu4/Tugas/Tugas3_4.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu4_Tugas;
+import java.util.Scanner;
+/**
+ *
+ * @author Fikri
+ */
+public class Tugas3_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int ank = 0,d=0,b=0;
+
+        
+        System.out.print("Masukkan Jumlah data :");
+        ank = scan.nextInt();
+        int [] list = new int[ank];
+        int min=list[0],max=list[0],avg=0;
+
+        
+        for (int i = 0; i < ank; i++) {
+            d = i+1;
+            System.out.print("Masukkan Nilai ke-"+d+" :");
+            b = scan.nextInt();
+            list[i] = b;
+        }
+        for (int i = 0; i < ank; i++) {
+            avg += list[i];
+            if (list[i] < max) {
+                min = list[i];
+            } else {
+                max = list[i];
+            }
+        }
+        System.out.println("Min :"+min);
+        System.out.println("Max :"+max);
+        avg = avg/list.length;
+        System.out.println("avg :"+avg);
+    }
+    
+}
