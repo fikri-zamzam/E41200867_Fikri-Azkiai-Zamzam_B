f324ac60b77aa1d922fce9037e8d6828d49dc44d
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-30 19:11:10 +0700

    Create Tugas2_4.java

---
+++ Minggu4/Tugas/Tugas2_4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu4_Tugas;
+
+/**
+ *
+ * @author Fikri
+ */
+public class Tugas2_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // UBAH INI KE DO WHILE YA LAPED
+        System.out.println("          Do While");
+        System.out.println(" Bilangan Kelipatan 2 (1 - 100)");
+        System.out.println("====================================");
+        int ank =1;
+        int x =0;
+        do {
+            System.out.print(ank + "  ");
+            ank*=2;
+            x++;
+        } while (x <7);
+        System.out.println("");
+        
+    }
+}
