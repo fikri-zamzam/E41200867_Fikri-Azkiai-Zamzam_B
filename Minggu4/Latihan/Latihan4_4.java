555e1f878bb8e4a97d4f847994406bd21dc6cf64
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-30 19:11:01 +0700

    Create Latihan4_4.java

---
+++ Minggu4/Latihan/Latihan4_4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minggu4_Latihan;
+
+/**
+ *
+ * @author Fikri
+ */
+public class Latihan4_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int angka[] = {3,2,1,4,3};
+        
+        for (int i : angka) {
+            System.out.print(i + " ");
+        }
+    }
+    
+}
