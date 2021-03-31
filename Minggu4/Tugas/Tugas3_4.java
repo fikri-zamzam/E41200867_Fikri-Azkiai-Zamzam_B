7481fe960642045f019bf1f253fdbea46d3f7c4c
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2021-03-31 21:39:00 +0700

    Update Tugas3_4.java

--- Minggu4/Tugas/Tugas3_4.java
+++ Minggu4/Tugas/Tugas3_4.java
@@ -15,16 +15,14 @@ public class Tugas3_4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
+        Scanner scan = new Scanner(System.in);      
         int ank = 0,d=0,b=0;
 
-        
         System.out.print("Masukkan Jumlah data :");
         ank = scan.nextInt();
         int [] list = new int[ank];
         int min=list[0],max=list[0],avg=0;
-
+        boolean t = false;
         
         for (int i = 0; i < ank; i++) {
             d = i+1;
@@ -36,14 +34,18 @@ public class Tugas3_4 {
             avg += list[i];
             if (list[i] < max) {
                 min = list[i];
+                t = true;
             } else {
                 max = list[i];
             }
+            if (!t) {
+                min = list[0];
+            }
         }
-        System.out.println("Min :"+min);
-        System.out.println("Max :"+max);
+        System.out.println("Nilai Terkecilnya Adalah :"+min);
+        System.out.println("Nilai Terbesarnya Adalah :"+max);
         avg = avg/list.length;
-        System.out.println("avg :"+avg);
+        System.out.println("Nilai Rata ratanya Adalah :"+avg);
     }
     
 }
